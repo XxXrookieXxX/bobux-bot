bbf80b319c3c19b4eff2378511173d1a6061f2a3
XxXrookieXxX <75860021+XxXrookieXxX@users.noreply.github.com>
2025-10-03 10:50:34 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,22 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Привет! Я в облаке и готов помочь!')
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Вы сказали: {update.message.text}')
+
+def main():
+    import os
+    TOKEN = os.environ.get('8420264615:AAE9JtYlJIEArImZSa4-hdxdpzsGvCkegbU')  # токен берём из переменной окружения
+
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
