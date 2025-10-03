c7d84cf7fda8f549d9dc572d704ddd8e306a8e7a
XxXrookieXxX <75860021+XxXrookieXxX@users.noreply.github.com>
2025-10-03 12:47:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,21 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+import requests
+
+def get_usd_rate():
+    url = "https://www.cbr-xml-daily.ru/daily_json.js"
+    response = requests.get(url).json()
+    return response["Valute"]["USD"]["Value"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Привет! Я в облаке и готов помочь!')
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'Вы сказали: {update.message.text}')
+
+async def usd_command(message: types.Message):
+    rate = get_usd_rate()
+    await message.answer(f"💵 По данным Центробанка РФ, курс доллара: {rate:.2f} руб.")
 
 def main():
     import os
